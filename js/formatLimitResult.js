d3ad7e399d617b2b2f27a0212bd51e5845da579a
shaolinjr <arthurclp@icloud.com>
2016-10-14 14:06:43 -0300

    Modifiquei a maneira de funcionamento do js, coloquei tudo inline no index para facilitar nossa vida

--- js/formatLimitResult.js
+++ js/formatLimitResult.js
@@ -7,11 +7,11 @@ class FormatLimitResult {
 		this.resultLenght = limitResult.length;
 		this.result = parseInt(limitResult);
 		if (this.result > 0){
-			this.result = `${this.result}/${pow(10,this.resultLenght)}`;		
+			this.result = `${this.result}/${pow(10,this.resultLenght)}`;
 		}else {
 			this.result =`${this.result}/${pow(10,this.resultLenght)}`;
 		}
 		return this.result;
-	}	
+	}
 
 }
