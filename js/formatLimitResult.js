8f805c04f5b5cff7d5600dc44e67017be4530dae
shaolinjr <arthurclp@icloud.com>
2016-10-14 00:01:53 -0300

    Inicio de desenvolvimento de site/aplicação para Auto Instrucional de Cálculo I, FUMEC 2016

---
+++ js/formatLimitResult.js
@@ -0,0 +1,17 @@
+class FormatLimitResult {
+
+	constructor(){
+	}
+
+	static decimalToFraction (limitResult){
+		this.resultLenght = limitResult.length;
+		this.result = parseInt(limitResult);
+		if (this.result > 0){
+			this.result = `${this.result}/${pow(10,this.resultLenght)}`;		
+		}else {
+			this.result =`${this.result}/${pow(10,this.resultLenght)}`;
+		}
+		return this.result;
+	}	
+
+}
